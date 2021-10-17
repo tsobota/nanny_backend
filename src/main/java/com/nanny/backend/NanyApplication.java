2e3abf968a47d7a8d61d3bf58e84278982e01e29
Tomasz Sobota <sobott01@stepstone.com>
2021-10-17 19:50:59 +0200

    Init commit

---
+++ src/main/java/com/nanny/backend/NanyApplication.java
@@ -0,0 +1,13 @@
+package com.nanny.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NanyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nanny/backend/NanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanny.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
